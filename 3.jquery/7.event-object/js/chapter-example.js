97150092a171609c4eea0490f32993f8e08a0f48
ZaneLaufunny <948495408@qq.com>
2017-11-06 14:57:35 +0800

    jquery练习

---
+++ 3.jquery/7.event-object/js/chapter-example.js
@@ -0,0 +1,26 @@
+/**
+* 功能：键盘切换图片显示
+**/
+/**** 页面加载完成后执行 ****/
+$(function() {
+	// 键盘控制图片显示切换
+	tabImageShow();
+});
+
+/**** 功能函数定义部分 ****/
+/**
+* 功能：键盘控制图片显示切换
+**/
+function tabImageShow() {
+	$(document).keydown(function(e) {
+		var $banner = $(".banner");
+		var $imgList = $banner.children("img");
+		// “←”是37，“→”是39
+		if(e.which === 37) {
+			$banner.prepend($imgList.last());
+		}
+		if(e.which === 39) {
+			$imgList.eq(0).appendTo(".banner");
+		}
+	});
+}
